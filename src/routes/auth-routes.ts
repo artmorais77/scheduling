ffa6ae3d58aee6f9181bc9739392bb231d3b90e5
artmorais77 <arturmorais.ald@gmail.com>
2025-11-17 07:04:56 -0300

    feat(ensureAuthenticated): adiciona middleware de autenticação

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,7 +1,8 @@
 import { AuthController } from "@/controllers/auth-controller"
+import { EnsureAuthenticated } from "@/middlewares/ensureAuthenticated"
 import { Router } from "express"
 
 export const authRoutes = Router()
 const authController = new AuthController()
 
-authRoutes.get("/", authController.auth)
+authRoutes.get("/",  EnsureAuthenticated,authController.auth)
